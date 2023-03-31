271d662bcadf84ddd9910961bd7a5130b5de2383
albert <amyik100@gmail.com>
2023-03-31 15:41:56 +0900

    first commit

---
+++ src/main/java/com/example/mavenmultimodulesample/MavenMultiModuleSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenmultimodulesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenMultiModuleSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MavenMultiModuleSampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/mavenmultimodulesample/MavenMultiModuleSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenmultimodulesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenMultiModuleSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
